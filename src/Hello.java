4894dc5e86fbe68db8c6eb4b8ca63a048b94a22d
TareqHasan18 <akmhasan18@gmail.com>
2024-12-10 20:56:17 -0500

    new update

--- src/Hello.java
+++ src/Hello.java
@@ -2,5 +2,6 @@ public class Hello {
 
     public static void main(String[] args) {
         System.out.println("Hello GitHub! This is another update");
+        System.out.println("Hello everyone! checking another sentence for github");
     }
 }
