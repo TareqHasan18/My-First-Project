eda81dc62ae29365de4dd5883391df978a7c6874
TareqHasan18 <akmhasan18@gmail.com>
2024-12-12 20:11:24 -0500

    Added if else statement

--- src/Hello.java
+++ src/Hello.java
@@ -1,12 +1,25 @@
 public class Hello {
-
     public static void main(String[] args) {
         System.out.println("Hello GitHub! This is another update");
         System.out.println("Hello everyone! checking another sentence for github");
 
         //This is just a basic for loop
-        for(int i = 0; i < 10; i++){
-            System.out.println(i);
+//        for(int i = 0; i < 10; i++){
+//            System.out.println(i);
+//        }
+
+        //write a if else statement which will determine achived grade for a student
+        int grade = 55;
+        if(grade > 91){
+            System.out.println("A+");
+        }else if(grade > 81 && grade <= 90){
+            System.out.println("A");
+        }else if(grade > 71 && grade <= 80){
+            System.out.println("B");
+        } else if (grade > 61 && grade <= 70) {
+            System.out.println("C");
+        }else {
+            System.out.println("D");
         }
     }
 }
